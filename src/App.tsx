83f4b3e6a480fe1a97b953fe18a2b6a753d73e9b
Yokorosan <etherhiroaki@hotmail.com>
2022-11-17 17:04:08 -0300

    adding responsivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import "../src/styles/Sass/App.scss";
 import { CompSearch } from "./components/search";
-
+import { GiCardRandom } from "react-icons/gi";
 import { MtgContext } from "./contexts/mtgcontext";
 
 function App() {
@@ -11,17 +11,16 @@ function App() {
     rules,
     newName,
     autoComplete,
-    getSpecificCard,
     setAutoComplete,
     setInputValue,
-    inputValue,
     setIsActive,
   } = useContext(MtgContext);
 
   return (
     <>
       <header>
-        <div className="App">
+        <h1>MTG Search Engine</h1>
+        <div className="headerDiv">
           <button onClick={() => getCommander()}>Random Commander</button>
           <CompSearch />
           {autoComplete.length !== 0 ? (
@@ -46,15 +45,18 @@ function App() {
       </header>
       <main>
         <div className="mainDiv">
-          <div className="mainDivPicture">
-            <h2>{card.name}</h2>
-            {card.image_uris ? (
+          {card.image_uris ? (
+            <div className="mainDivPicture">
+              <h2>{card.name}</h2>
               <img src={card?.image_uris.normal} alt={card?.name} />
-            ) : (
-              <>
+            </div>
+          ) : (
+            <div className="placeholderDiv">
+              <GiCardRandom />
+              <div>
                 <p>
                   Não Encontramos uma imagem, mas talvez você tenha mais sorte
-                  no
+                  no{" "}
                   <a
                     href={`https://gatherer.wizards.com/Pages/Card/Details.aspx?name=${card.name}`}
                     target="_blank"
@@ -63,11 +65,12 @@ function App() {
                     Gatherer
                   </a>
                 </p>
-              </>
-            )}
-          </div>
-          <div className="mainDivRules">
-            {rules.length !== 0 ? (
+              </div>
+            </div>
+          )}
+
+          {rules.length !== 0 ? (
+            <div className="mainDivRules">
               <ul>
                 {rules?.map((element, index) => {
                   return (
@@ -80,8 +83,11 @@ function App() {
                   );
                 })}
               </ul>
-            ) : (
-              <>
+            </div>
+          ) : (
+            <div className="placeholderDiv">
+              <GiCardRandom />
+              <div>
                 <p>
                   Não Encontramos rulings, mas talvez você tenha mais sorte no{" "}
                   <a
@@ -92,16 +98,19 @@ function App() {
                     Gatherer
                   </a>
                 </p>
-              </>
-            )}
-          </div>
+              </div>
+            </div>
+          )}
         </div>
-        <div>
-          {!newName ? (
-            <></>
-          ) : (
-            <>
-              <p>Sites</p>
+        <div></div>
+      </main>
+      {!newName ? (
+        <></>
+      ) : (
+        <footer>
+          <h1>Sites</h1>
+          <section>
+            <div>
               <p>Deck Builder</p>
               <a
                 href="https://www.archidekt.com"
@@ -110,18 +119,30 @@ function App() {
               >
                 ArchiDekt
               </a>
+            </div>
+            <div>
               <p>Reference</p>
-              <a
-                href={`https://edhrec.com/commanders/${newName}`}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Edhrec
-              </a>
-            </>
-          )}
-        </div>
-      </main>
+              {card.type_line.includes("Legendary Creature") ? (
+                <a
+                  href={`https://edhrec.com/commanders/${newName}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Edhrec
+                </a>
+              ) : (
+                <a
+                  href={`https://edhrec.com/cards/${newName}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Edhrec
+                </a>
+              )}
+            </div>
+          </section>
+        </footer>
+      )}
     </>
   );
 }
